d5e35b4c33b32c9eee90bc08c8e23fe41ae6bc35
blueoasis <blueoasis@users.noreply.github.com>
2022-05-03 13:35:09 -0600

    Created Notes, Recipe, Ingredient, Difficulty, UnitOfMeasure entities

---
+++ src/main/java/guru/springframework/model/Recipe.java
@@ -0,0 +1,129 @@
+package guru.springframework.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Note note;
+
+    @Lob
+    private Byte[] image;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients = new HashSet<>();
+
+    private String description;
+    private Integer prepItem;
+    private Integer cookItem;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+
+    @Enumerated(EnumType.STRING)
+    private Difficulty difficulty;
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public Note getNote() {
+        return note;
+    }
+
+    public void setNote(Note note) {
+        this.note = note;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrepItem() {
+        return prepItem;
+    }
+
+    public void setPrepItem(Integer prepItem) {
+        this.prepItem = prepItem;
+    }
+
+    public Integer getCookItem() {
+        return cookItem;
+    }
+
+    public void setCookItem(Integer cookItem) {
+        this.cookItem = cookItem;
+    }
+
+    public Integer getServings() {
+        return servings;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+}
---
+++ src/main/java/guru/springframework/model/UnitOfMeasure.java
@@ -0,0 +1,25 @@
+package guru.springframework.model;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+}
